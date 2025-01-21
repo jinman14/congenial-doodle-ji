5381a2a32125ce05ee9038b339165be3d221a435
Jacob Inman <jrinman6295@gmail.com>
2025-01-21 08:22:03 -0800

    visitor can add preferences

--- lib/visitor.rb
+++ lib/visitor.rb
@@ -1,10 +1,14 @@
 class Visitor
-  attr_reader :name, :height, :spending_money, :preference
+  attr_reader :name, :height, :spending_money, :preferences
 
   def initialize(name, height, spending_money)
     @name = name
     @height = height
     @spending_money = spending_money.delete('$').to_i
-    @preference = []
+    @preferences = []
+  end
+
+  def add_preference(trait)
+    @preferences << trait
   end
 end
--- spec/visitor_spec.rb
+++ spec/visitor_spec.rb
@@ -15,12 +15,12 @@ RSpec.describe Visitor do
       expect(@visitor1.name).to eq 'Bruce'
       expect(@visitor1.height).to eq 54
       expect(@visitor1.spending_money).to eq 10
-      expect(@visitor1.preference).to eq []
+      expect(@visitor1.preferences).to eq []
     end
   end
 
   describe '#add_preference' do
-    xit 'can add preferences' do
+    it 'can add preferences' do
       @visitor1.add_preference(:gentle)
       @visitor1.add_preference(:thrilling)
 
